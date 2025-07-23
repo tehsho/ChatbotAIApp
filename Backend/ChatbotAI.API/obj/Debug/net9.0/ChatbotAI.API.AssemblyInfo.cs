a96278e1393c08d501e7546879ecf3b39305cd8c
tehsho <tehsho@gmail.com>
2025-07-23 09:42:10 -0400

    Initial commit with ChatbotAI project

--- Backend/ChatbotAI.API/obj/Debug/net9.0/ChatbotAI.API.AssemblyInfo.cs
+++ Backend/ChatbotAI.API/obj/Debug/net9.0/ChatbotAI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatbotAI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c418c961203ef30b7781deeab375607d5d7bbd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatbotAI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatbotAI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
